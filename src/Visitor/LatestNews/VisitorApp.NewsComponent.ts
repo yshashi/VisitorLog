5d45927be3c3b9a6dbf2d7a8e28da72b04b908a4
Sashi <yshashi30@gmail.com>
2020-08-20 10:15:47 +0530

    Update VisitorApp.NewsComponent.ts

--- src/Visitor/LatestNews/VisitorApp.NewsComponent.ts
+++ src/Visitor/LatestNews/VisitorApp.NewsComponent.ts
@@ -8,7 +8,7 @@ export class NewsComponent {
 
     public lastestNewsList: any = [];
     constructor(public http: HttpClient) {
-        this.http.get<any>("http://newsapi.org/v2/top-headlines?country=in&apiKey=c2571d3d3f6a4194a3f15f4f1d5f1249")
+        this.http.get<any>("https://newsapi.org/v2/top-headlines?country=in&apiKey=c2571d3d3f6a4194a3f15f4f1d5f1249")
             .subscribe(res => {
                 this.lastestNewsList = res.articles
                 console.log(res);
